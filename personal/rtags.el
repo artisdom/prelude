b958387a3a54b64df88c4a32976d04ef5755e298
Xigang Feng <xigang.feng@ericsson.com>
2017-12-17 13:03:11 +0800

    mv rtags part to one file.
    Change-Id: I19d89c403845dab4edc3ee729b5617d77946d5ff

---
+++ personal/rtags.el
@@ -0,0 +1,90 @@
+(setenv "PATH" (concat (getenv "PATH") ":/home/ezfenxi/install/rtags/bin"))
+(setq exec-path (append exec-path '("/home/ezfenxi/install/rtags/bin")))
+
+
+;; rtags
+
+(require 'rtags)
+
+(defun use-rtags (&optional useFileManager)
+  (and (rtags-executable-find "rc")
+       (cond ((not (gtags-get-rootpath)) t)
+             ((and (not (eq major-mode 'c++-mode))
+                   (not (eq major-mode 'c-mode))) (rtags-has-filemanager))
+             (useFileManager (rtags-has-filemanager))
+             (t (rtags-is-indexed)))))
+
+(defun tags-find-symbol-at-point (&optional prefix)
+  (interactive "P")
+  (if (and (not (rtags-find-symbol-at-point prefix)) rtags-last-request-not-indexed)
+      (gtags-find-tag)))
+(defun tags-find-references-at-point (&optional prefix)
+  (interactive "P")
+  (if (and (not (rtags-find-references-at-point prefix)) rtags-last-request-not-indexed)
+      (gtags-find-rtag)))
+(defun tags-find-symbol ()
+  (interactive)
+  (call-interactively (if (use-rtags) 'rtags-find-symbol 'gtags-find-symbol)))
+(defun tags-find-references ()
+  (interactive)
+  (call-interactively (if (use-rtags) 'rtags-find-references 'gtags-find-rtag)))
+(defun tags-find-file ()
+  (interactive)
+  (call-interactively (if (use-rtags t) 'rtags-find-file 'gtags-find-file)))
+(defun tags-imenu ()
+  (interactive)
+  (call-interactively (if (use-rtags t) 'rtags-imenu 'idomenu)))
+
+(define-key c-mode-base-map (kbd "M-.") (function tags-find-symbol-at-point))
+(define-key c-mode-base-map (kbd "M-,") (function tags-find-references-at-point))
+(define-key c-mode-base-map (kbd "M-;") (function tags-find-file))
+(define-key c-mode-base-map (kbd "C-.") (function tags-find-symbol))
+(define-key c-mode-base-map (kbd "C-,") (function tags-find-references))
+(define-key c-mode-base-map (kbd "C-<") (function rtags-find-virtuals-at-point))
+(define-key c-mode-base-map (kbd "M-i") (function tags-imenu))
+
+(define-key global-map (kbd "M-.") (function tags-find-symbol-at-point))
+(define-key global-map (kbd "M-,") (function tags-find-references-at-point))
+(define-key global-map (kbd "M-;") (function tags-find-file))
+(define-key global-map (kbd "C-.") (function tags-find-symbol))
+(define-key global-map (kbd "C-,") (function tags-find-references))
+(define-key global-map (kbd "C-<") (function rtags-find-virtuals-at-point))
+(define-key global-map (kbd "M-i") (function tags-imenu))
+
+(require 'company)
+(require 'company-rtags)
+
+(setq rtags-autostart-diagnostics t)
+(setq rtags-completions-enabled t)
+(rtags-diagnostics)
+(push 'company-rtags company-backends)
+(global-company-mode)
+(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+
+(require 'helm-rtags)
+(setq rtags-use-helm t)
+
+(require 'flycheck)
+(require 'flycheck-rtags)
+
+(defun my-flycheck-rtags-setup ()
+  (flycheck-select-checker 'rtags)
+  (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+  (setq-local flycheck-check-syntax-automatically nil))
+(add-hook 'c-mode-hook #'my-flycheck-rtags-setup)
+(add-hook 'c++-mode-hook #'my-flycheck-rtags-setup)
+(add-hook 'objc-mode-hook #'my-flycheck-rtags-setup)
+
+(setq rtags-display-result-backend 'helm)
+
+(define-key prelude-mode-map (kbd "C-c r") nil)
+(rtags-enable-standard-keybindings c-mode-base-map "\C-cr")
+
+(setq rtags-close-taglist-on-selection t)
+(setq rtags-find-file-case-insensitive t)
+;(custom-set-variables '(rtags-close-taglist-on-selection nil))
+;or you can just use setq, since the variable doesn't have a setter defined:
+;(setq cperl-indent-parens-as-block t)
+
+;; Set always complete immediately
+(setq company-idle-delay 0)
