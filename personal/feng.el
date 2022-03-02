f2fc76572f6d6f5ddf284995fc982dde3ed9cc15
Ted Feng <Xigang.Feng@gallagher.com>
2022-03-03 11:07:49 +1300

    Support editing WSL filesystem from Windows Emacs.

--- personal/feng.el
+++ personal/feng.el
@@ -412,3 +412,14 @@
 
 ;(require 'org-tempo)
 
+(when (eq system-type 'windows-nt)
+  (defun fp/ignore-wsl-acls (orig-fun &rest args)
+    "Ignore ACLs on WSL. WSL does not provide an ACL, but emacs
+expects there to be one before saving any file. Without this
+advice, files on WSL can not be saved."
+    (if (string-match-p "^//wsl\$/" (car args))
+        (progn (message "ignoring wsl acls") "")
+      (apply orig-fun args)))
+
+  (advice-add 'file-acl :around 'fp/ignore-wsl-acls))
+
